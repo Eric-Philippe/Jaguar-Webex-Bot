88463196e5761ff0fd2043d998f49b33dc88ae4e
Eric-Philippe <ericphlpp@gmail.com>
2024-05-02 14:49:51 +0200

    Add Custom Config Manager

---
+++ config/CustomConfig.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+
+const { ORDER, ANNOUNCEMENT_TIME } = require("./config.json").CONFIG;
+
+/**
+ * Class to handle the configuration that is
+ * able to be changed by the user without restarting the bot
+ */
+class CustomConfig {
+  /**
+   * Create a new CustomConfig
+   */
+  constructor() {
+    this.annoucement_time = ANNOUNCEMENT_TIME;
+    this.order = ORDER;
+  }
+
+  /**
+   * @returns {string} The time of the annoucement
+   */
+  getAnnoucementTime() {
+    return this.annoucement_time;
+  }
+
+  /**
+   * @returns {'ASC' | 'DESC'} The order of the announcements
+   */
+  getOrder() {
+    return this.order;
+  }
+
+  /**
+   * Set the time of the annoucement
+   * @param {string} time
+   */
+  setAnnoucementTime(time) {
+    // Must be an hour like so : "HH:MM"
+    if (!/^\d{2}:\d{2}$/.test(time)) {
+      throw new Error("Time must be in the format HH:MM");
+    }
+    this.annoucement_time = time;
+
+    return this.saveToJson();
+  }
+
+  /**
+   * Set the order of the announcements
+   * @param {'ASC' | 'DESC'} order - Must be 'ASC' or 'DESC
+   */
+  async setOrder(order) {
+    // Must be "ASC" or "DESC
+    if (!["ASC", "DESC"].includes(order)) {
+      throw new Error("Order must be 'ASC' or 'DESC'");
+    }
+    this.order = order;
+
+    return await this.saveToJson();
+  }
+
+  async saveToJson() {
+    const data = JSON.stringify(
+      {
+        CONFIG: {
+          ORDER: this.order,
+          ANNOUNCEMENT_TIME: this.annoucement_time,
+        },
+      },
+      null,
+      2
+    );
+
+    await fs.promises.writeFile("./config/config.json", data);
+
+    return data;
+  }
+}
+
+const configInstance = new CustomConfig();
+
+module.exports = configInstance;
--- config/config.js
+++ config/config.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 
 const WEBHOOKURL = process.env.WEBHOOKURL;
 const TOKEN = process.env.BOTTOKEN;
+const MAIN_GROUPID = process.env.MAIN_GROUPID;
 const PORT = process.env.PORT;
 var DEV = process.env.DEV;
 var DEBUG = process.env.DEBUG;
 
-if (!WEBHOOKURL || !TOKEN || !PORT) {
+if (!WEBHOOKURL || !TOKEN || !PORT || !MAIN_GROUPID) {
   console.error("Please provide all the required environment variables.");
   process.exit(1);
 }
@@ -22,6 +23,7 @@ DEBUG =
 const FULL_CONFIG = {
   webhookUrl: WEBHOOKURL,
   token: TOKEN,
+  mainGroupId: MAIN_GROUPID,
   port: PORT,
   dev: DEV,
   debug: DEBUG,
@@ -30,6 +32,7 @@ const FULL_CONFIG = {
 module.exports = {
   WEBHOOKURL,
   TOKEN,
+  MAIN_GROUPID,
   PORT,
   DEV,
   DEBUG,
