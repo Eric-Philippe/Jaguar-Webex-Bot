a1d1d35292bfa98160b99aee5526bdc4a11b9a19
Eric-Philippe <ericphlpp@gmail.com>
2024-05-03 09:56:19 +0200

    feat: Assign Command

--- commands/Assign.js
+++ commands/Assign.js
@@ -16,18 +16,22 @@ const assign = {
    * @param {string[]} args
    */
   handler: async (bot, trigger, args) => {
+    // Check if the user entered a name
     if (args.length <= 1) return bot.say("❌ | Il faut au moins un argument !");
     const firstName = args[1];
 
+    // Find the member from the webex space
     let member = await Utils.getMemberByName(bot, firstName);
     if (!member) return bot.say("❌ | Utilisateur non trouvé !");
 
+    // Find the user in the database
     let user = await UserServices.getUser(member.personId);
     if (!user)
       return bot.say(
         `❌ | L'utilisateur ${firstName} n'est pas dans la liste pour la boîte commune ! Merci de l'ajouter avec la commande \`ajouter\` !`
       );
 
+    // Check if the user is already pointed
     if (user.pointed) {
       await UserServices.setPointed(user.id, false);
       bot.say("markdown", `✅ | **${user.firstName} ${user.lastName}** a bien été désaffecté de la boîte commune !`);
