6824bc9746dc2cf7366482fdd2da3ff7d01f179c
Eric-Philippe <ericphlpp@gmail.com>
2025-07-29 13:36:03 +0200

    Remove uselesscomment

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PORT = process.env.DB_PORT || 5432;
-const DB_NAME = process.env.DB_NAME || "jaguar"; // Default database name
+const DB_NAME = process.env.DB_NAME || "jaguar";
 const DB_USER = process.env.DB_USER || "postgres";
 const DB_PASSWORD = process.env.DB_PASSWORD || "password";
 
